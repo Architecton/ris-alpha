95660891c9a4b49fd7bdc07d3603a6cb1084e6a2
miha987 <zadravecmiha987@gmail.com>
2019-03-09 11:19:48 +0100

    implementacija gibanja v kvadratu in krogu

--- alpha2/scripts/service.py
+++ alpha2/scripts/service.py
@@ -1,36 +1,74 @@
 #!/usr/bin/env python
 
-from alpha2.srv import TurtleManager
 import rospy
+from alpha2.srv import TurtleManager
+from geometry_msgs.msg import Twist
 
-def handle_rectangular_mvm(duration):
+def handle_rectangular_mvm(step):
+    twist = Twist()
+    twist.linear.x = 0.1
+    step = step % 20
+
+    if step % 5 == 0:
+        twist.linear.x = 0
+        twist.angular.z = 1.57 #(90 / 360) * 2 * 3.14
+
+    return twist
+
+def handle_triangular_mvm(step):
     pass
 
-def handle_triangular_mvm(duration):
-    pass
+def handle_circular_mvm(step):
+    twist = Twist()
+    twist.linear.x = 0.07
 
-def handle_circular_mvm(duration):
-    pass
+    if step % 2 == 0:
+        twist.linear.x = 0
+        twist.angular.z = 0.157
 
-def handle_random_mvm(duration):
+    return twist
+
+def handle_random_mvm(step):
     pass    
 
 def handle_movement_manager(req):
-    if req.trajection == 'rectangle':
-        handle_rectangular_mvm(req.duration)
-    elif req.trajection == 'triangle':
-        handle_triangular_mvm(req.duration)
-    elif req.trajection == 'circle':
-        handle_circular_mvm(req.duration)
-    elif req.trajection == 'random':
-        handle_random_mvm(req.duration) 
 
-    return "placeholder"
+    pub = rospy.Publisher('turtle1/cmd_vel', Twist, queue_size = 1000)
+
+    step = 0.0
+
+    beginTime = rospy.get_time() # to vraca sekunde
+    endTime = beginTime + req.duration # predvidevamo da je tudi req.duration v sekundah
+
+    print 'Got request' 
+    print 'Begin time: %f' % beginTime
+    print 'End time: %f' % endTime
+
+    rate = rospy.Rate(1)
+
+    while rospy.get_time() < endTime:
+        if req.trajection == 'rectangle':
+            twist = handle_rectangular_mvm(step)
+        elif req.trajection == 'triangle':
+            twist = handle_triangular_mvm(step)
+        elif req.trajection == 'circle':
+            twist = handle_circular_mvm(step)
+        elif req.trajection == 'random':
+            twist = handle_random_mvm(step) 
+        
+        pub.publish(twist)
+        step = step + 1.0
+        rate.sleep()
+
+
+    return req.trajection
 
 def movement_manager_server():
     rospy.init_node('movement_manager_server')
     s = rospy.Service('movement_manager', TurtleManager, handle_movement_manager)
+
     print('Ready to handle requests.')
+
     rospy.spin()
 
 if __name__ == '__main__':
