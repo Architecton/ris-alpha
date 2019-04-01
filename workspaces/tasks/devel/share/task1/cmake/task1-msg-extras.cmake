a3c9617561b5b561cfdcb667c69fac9ff232846d
Damjan Kalšan <dk7933@student.uni-lj.si>
2019-04-01 16:45:52 +0200

    Added ring detector

---
+++ workspaces/tasks/devel/share/task1/cmake/task1-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(task1_MESSAGE_FILES "/home/dkalsan/ris-alpha/workspaces/tasks/src/task1/msg/EllipseData.msg")
+set(task1_SERVICE_FILES "")
---
+++ workspaces/tasks/devel/share/task1/cmake/task1-msg-paths.cmake
@@ -0,0 +1,4 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
+
+set(task1_MSG_INCLUDE_DIRS "/home/dkalsan/ris-alpha/workspaces/tasks/src/task1/msg")
+set(task1_MSG_DEPENDENCIES std_msgs)
