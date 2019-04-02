8906e69898b35f11f281bd56bee794ec01c58015
Jernej Vivod <je.vivod@gmail.com>
2019-04-02 13:45:17 +0200

    Fix issues and add speak functionallity

--- workspaces/tasks/src/task1/scripts/main.py
+++ workspaces/tasks/src/task1/scripts/main.py
@@ -16,8 +16,10 @@ import tf2_geometry_msgs
 from geometry_msgs.msg import Point, Vector3, PoseStamped
 
 from targetmarker import TargetMarker
-
 from task1.srv import EllipseLocator
+
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
 
 import time
 
@@ -32,52 +34,55 @@ import time
 rospy.init_node('main')
 
 # Initialize TargetMarker instance.
-#tm = TargetMarker()
-
-# example of marking a target:
-# tm.push_position(np.array([0.2, 0.3, 0.5]))
-
+tm = TargetMarker()
 
 # Initialize action clients
 ac_chkpnts = actionlib.SimpleActionClient("move_base", MoveBaseAction)
 ac_ellipses = actionlib.SimpleActionClient("move_base", MoveBaseAction)
 
 # Initialize checkpoints array.
+# TODO: integrate with Miha's part.
 checkpoints = np.array([[-1.276, 2.121, 0.850], [0.421, 0.166, 0.597], [2.195, 0.723, 0.999], [1.832, 2.986, 0.882], [0.036, 2.124, 0.855]])
-checkpoint_ctr = 0
+checkpoint_ctr = 0  # Initialize found ellipses counter.
 
 # Initialize found ellipses storage.
-resolved_ell = np.empty((0, 2), dtype=float)
+# First two fields store the coordinates of the ellipse center. The third field stores the perpendicular angle.
+resolved_ell = np.empty((0, 3), dtype=float)
+resolved_ell_ctr = 0  # Initialize resolved ellipses counter.
 
 # Set distance threshold to consider ellipse as unresolved.
-DISTINCT_ELL_THRESH = 1.0
+# TODO: empirically determine best threshold.
+DISTINCT_ELL_THRESH = 0.7
 
-tf2_buffer = tf2_ros.Buffer()  # initialize coordinate transforms buffer.
+# Initialize coordinate transforms buffer.
+tf2_buffer = tf2_ros.Buffer()
 tf2_listener = tf2_ros.TransformListener(tf2_buffer)
 
 # Get robot position in map coordinates.
-time.sleep(2)
+rospy.sleep(1)  # Wait for cache to fill.
 trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
 robot_pos = np.array([trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z]) 
 
-# Wait for ellipse data buffer query service to come online.
+# Wait for ellipse data buffer query service to come online and make a proxy function.
 rospy.wait_for_service('ellipse_locator')
 ellipse_locator = rospy.ServiceProxy('ellipse_locator', EllipseLocator)
 
+# Initialize sound node.
+rospy.init_node('say', anonymous = True)
+soundhandle = SoundClient()
+rospy.sleep(1)
+voice = 'voice_kal_diphone'
+volume = 1.0
+
+
 ### /INITIALIZATIONS ###
-
-
-
 
 
 # While there are checkpoints left **main search loop**
 while checkpoints.shape[0] > 0:
 
     # Get index of closest checkpoint.
-    if checkpoints.shape[0] > 1:
-    	idx_nxt = np.argmin((lambda x1, x2: np.sum(np.abs(x1 - x2)**2, 1)**(1/2))(robot_pos, checkpoints))
-    else:
-    	idx_nxt = np.argmin((lambda x1, x2: np.sum(np.abs(x1 - x2)**2)**(1/2))(robot_pos, checkpoints))
+    idx_nxt = np.argmin((lambda x1, x2: np.sqrt(np.sum(np.abs(x1 - x2)**2, 1)))(robot_pos, checkpoints))
 
     
     # Create goal for next checkpoint.
@@ -90,13 +95,13 @@ while checkpoints.shape[0] > 0:
 
     goal_chkpnt_status = GoalStatus.LOST  # Set status for next checkpoint goal.
     ac_chkpnts.send_goal(goal_chkpt) # Send checkpoint goal.
-    rospy.loginfo("Resolving checkpoint {0}".format(idx_nxt))
+    rospy.loginfo("Resolving checkpoint {0}".format(checkpoint_ctr))
 
     # Loop for next checkpoint goal.
     while not goal_chkpnt_status == GoalStatus.SUCCEEDED:
 
         # Check if any ellipses if buffer.
-        try:
+        try:  # Initialize resolved ellipses counter.
             # Query into ellipse data buffer.
             ellipse_data = ellipse_locator()
             
@@ -104,7 +109,7 @@ while checkpoints.shape[0] > 0:
             if ellipse_data:
 
                 # Check if ellipse had not yet been resolved.
-                if not np.any((lambda x1, x2: np.sum(np.abs(x1 - x2)**2)**(1/2))(np.array([ellipse_data[0], ellipse_data[1], ellipse_data[2]]), resolved_ell) > DISTINCT_ELL_THRESH):
+                if not np.any((lambda x1, x2: np.sqrt(np.sum(np.abs(x1 - x2)**2, 1)))(np.array([ellipse_data[0], ellipse_data[1], ellipse_data[5]]), resolved_ell) < DISTINCT_ELL_THRESH):
                  
 
 
@@ -120,8 +125,7 @@ while checkpoints.shape[0] > 0:
                     ### /DEBUG PLOT ###
 
 
-                    """
-                    
+
                     # Create goal to approach found ellipse.
                     goal_ell = MoveBaseGoal()
                     goal_ell.target_pose.header.frame_id = "map"
@@ -142,28 +146,30 @@ while checkpoints.shape[0] > 0:
                         goal_nxt_ell_status = ac_ellipses.get_state()
 
                         # Handle abortions.
-                        if goal_state == GoalStatus.ABORTED or goal_state == GoalStatus.REJECTED: 
+                        if goal_nxt_ell_status == GoalStatus.ABORTED or goal_nxt_ell_status == GoalStatus.REJECTED: 
                             rospy.loginfo("Ellipse resolution goal aborted")
                             break
 
-                    # TODO speak
-                    print("Ellipse resolved")
+                        # If ellipse resolved, speak.
+                        if goal_nxt_ell_status == GoalStatus.SUCCEEDED:
+                            soundhandle.say("Target number {0} resolved.".format(resolved_ell_ctr), voice, volume)
+                            print "Target number {0} resolved.".format(resolved_ell_ctr)
+
 
                     # Add ellipse to array of resolved ellipses.
-                    resolved_ell = np.vstack((resolved_ell, np.array([ellipse_data[0], ellipse_data[1], ellipse_data[2]])))
+                    resolved_ell = np.vstack((resolved_ell, np.array([ellipse_data[0], ellipse_data[1], ellipse_data[5]])))
+                    resolved_ell_ctr += 1
                     # Resend preempted checkpoint goal.
-                    ac_chkpnts.send_goal(goal)
-
-                    """
-
+                    ac_chkpnts.send_goal(goal_chkpt)
                 else:
                     pass
+            else:
+                pass
         except rospy.ServiceException, e:
-            print "Service call failed: {0}".format(e)
+            print "Ellipse locator service call failed: {0}".format(e)
         
-        """
 
-        ac_chkpnts.wait_for_result(rospy.Duration(1))
+        ac_chkpnts.wait_for_result(rospy.Duration(0.1))
 
         # Get checkpoint resolution goal status.
         goal_chkpnt_status = ac_chkpnts.get_state()
@@ -173,8 +179,6 @@ while checkpoints.shape[0] > 0:
             rospy.loginfo("Checkpoint resolution goal aborted")
             break
 
-
-    
     # Remove checkpoint from checkpoints array
     checkpoints = np.delete(checkpoints, (idx_nxt), axis=0)
 
