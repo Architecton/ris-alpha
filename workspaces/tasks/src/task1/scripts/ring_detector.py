d26cf7626f7c42d03426f63a83b47c60e993a496
Damjan Kalšan <dk7933@student.uni-lj.si>
2019-04-04 00:18:49 +0200

    Commented code and tried some new things

--- workspaces/tasks/src/task1/scripts/ring_detector.py
+++ workspaces/tasks/src/task1/scripts/ring_detector.py
@@ -3,8 +3,13 @@
 import rospy
 import cv2
 import numpy as np
+import scipy as sp
 import tf2_geometry_msgs
 import tf2_ros
+import time
+from skimage import color, restoration
+from skimage import img_as_ubyte
+from scipy.signal import convolve2d as conv2
 from sensor_msgs.msg import Image
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PointStamped, Vector3, Pose
@@ -27,27 +32,26 @@ class The_Ring:
         self.scan_angle_increment = 0.0  
         self.in_process = 0
 
+        # Initialize search boundaries for the ellipses and their centres
+        self.upp_bnd_elps = 160 # ELLIPSE BOUNDARY: Offset from the top of the image
+        self.low_bnd_elps = 360 # ELLIPSE BOUNDARY: Offset from the top of the image
+        self.upp_bnd_ctr = 85 # CENTRE BOUNDARY: Offset from the the upp_bnd_elps
+        self.low_bnd_ctr = 135 # CENTRE BOUNDARY: Offset from the the upp_bnd_elps
+
         # An object we use for converting images between ROS format and OpenCV format
         self.bridge = CvBridge()
 
-        # A help variable for holding the dimensions of the image
-        self.dims = (0, 0, 0)
-
-        # Subscribe to the image and/or depth topic
+        # Subscribe to the RGB image topic
         self.image_sub = rospy.Subscriber("/camera/rgb/image_raw", Image, self.image_callback)
 
-        # TODO: Add ring publisher
+        # Add ring publisher
         self.rings_pub = rospy.Publisher('rings', EllipseData, queue_size=1000)
 
         # Subsribe to depth laser
-        self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)
-
-        # Object we use for transforming between coordinate frames
-        self.tf_buf = tf2_ros.Buffer()
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buf)      
-
+        self.laser_sub = rospy.Subscriber("/scan", LaserScan, self.scan_callback)   
 
     def image_callback(self,data):
+
         timestamp = rospy.Time.now().to_time()
         self.in_process = 1     
 
@@ -61,6 +65,25 @@ class The_Ring:
 
         # Tranform image to grayscale
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Following line used with Weiner filtering
+        # img = color.rgb2gray(img_original)
+
+        # Apply the median filter to remove noise
+        # img = cv2.medianBlur(img, 5)
+
+        # Apply sharpening (approx 0.01s additional processing time)
+        # kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+        # img = cv2.filter2D(img, -1, kernel)
+
+        # Take only subset of image inside the lower and upper ellipse boundary
+        img = img[self.upp_bnd_elps:self.low_bnd_elps, 0:len(img)]
+
+        # COMPUTATIONALY EXPENSIVE: Wiener filtering to deblur the image; 0.3s additional computation time
+        # psf = np.ones((5, 5)) / 25
+        # img = conv2(img, psf, 'same')
+        # img += 0.1 * img.std() * np.random.standard_normal(img.shape)
+        # img, _ = restoration.unsupervised_wiener(img, psf)
+        # img = img_as_ubyte(img)
 
         # Automatic parameter computation for canny
         sigma = 0.33
@@ -68,8 +91,9 @@ class The_Ring:
         lower = int(max(0, (1.0 - sigma) * v))
         upper = int(min(255, (1.0 + sigma) * v))
 
-        #upper, _ = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        #lower = 0.5 * upper
+        # Setting thresholds using the global otsu
+        # upper, _ = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # lower = 0.5 * upper
 
         # Find edges with canny
         edges = cv2.Canny(img, lower, upper)
@@ -78,21 +102,33 @@ class The_Ring:
         img2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
         # DEVONLY: Draw contours
-        # cv2.drawContours(img_original, contours, -1, (255, 0, 0), 3)
+        # cv2.drawContours(img, contours, -1, (255, 0, 0), 3)
+
+        # DEVONLY: Draw constraints for looking for ellipses
+        # cv2.line(img_original, (0, self.upp_bnd_elps), (640, self.upp_bnd_elps), (100, 100, 255), 2)
+        # cv2.line(img_original, (0, self.low_bnd_elps), (640, self.low_bnd_elps), (100, 100, 255), 2)
+
+        # DEVONLY: Only accept centers that are in an even smaller constrained area
+        # cv2.line(img_original, (0, self.upp_bnd_ctr + self.upp_bnd_elps), (640, self.upp_bnd_ctr + self.upp_bnd_elps), (40, 40, 255), 2)
+        # cv2.line(img_original, (0, self.low_bnd_ctr + self.upp_bnd_elps), (640, self.low_bnd_ctr + self.upp_bnd_elps), (40, 40, 255), 2)
 
         # Fit elipses to all extracted contours
         elps = []
         for cnt in contours:
             # Threshold for size of ellipses (number of pixels in contour)
-            if cnt.shape[0] >= 50:
-                # Checking contour ratio
+            if cnt.shape[0] >= 40:
+                # Checking ellipse shape and whether the centre is within the search boundaries
                 x,y,w,h = cv2.boundingRect(cnt)
                 aspect_ratio = min(float(w)/h, float(h)/w)
 
-                if(aspect_ratio > 0.9):
+                if(aspect_ratio > 0.9 and y+h/2 >= self.upp_bnd_ctr and y+h/2 <= self.low_bnd_ctr):
                     ellipse = cv2.fitEllipse(cnt)
                     elps.append(ellipse)
                     cv2.ellipse(img, ellipse, (0, 255, 0))
+
+                    # DEVONLY: Draw the centre of detected ellipse
+                    # cv2.line(img_original, (int(x + w/2), int(self.upp_bnd_elps + y + h/2)), (int(x + w/2), int(self.upp_bnd_elps + y + h/2)), (0, 0, 255), 10)
+
 
         # Find two elipses with same centers
         candidates = []
@@ -102,10 +138,21 @@ class The_Ring:
                 e2 = elps[m]
                 dist = np.sqrt(((e1[0][0] - e2[0][0]) ** 2 + (e1[0][1] - e2[0][1]) ** 2))
 
-                if dist < 5:
-                    # TODO: Preprocess the candidates that are the same candidate but detected multiple times
-                    candidates.append((e1,e2))      
+                # Check if we detected this candidate already and the ratio between the ellipses should correspond to a ring
+                is_unique = False
 
+                # if dist < 5:
+                #     for can in candidates:
+                #         dist2 = np.sqrt(((e1[0][0] - can[0][0][0]) ** 2 + (e1[0][1] - can[0][0][1]) ** 2))
+                #         
+                #         if(dist2 > 10):
+                #             is_unique = True
+                # 
+                #     if ((is_unique or len(candidates) == 0) and (1.2 < max(e1[1][0]/e2[1][0], e2[1][0]/e1[1][0]) < 1.5)):
+                #         candidates.append((e1,e2))   
+
+                if dist < 5:
+                    candidates.append((e1, e2))                                   
 
         # Build an array of detected Rings
         ed = EllipseData()
@@ -120,15 +167,13 @@ class The_Ring:
             e2 = c[1]
 
             # DEVONLY: Draw the detections
-            cv2.ellipse(img_original, e1, (0, 255, 0), 2)
-            cv2.ellipse(img_original, e2, (0, 255, 0), 2)
+            cv2.ellipse(img_original[self.upp_bnd_elps:self.low_bnd_elps, 0:len(img_original)], e1, (0, 255, 0), 2)
+            cv2.ellipse(img_original[self.upp_bnd_elps:self.low_bnd_elps, 0:len(img_original)], e2, (0, 255, 0), 2)
 
-            center = (e1[0][0], e1[0][1])
+            center = (e1[0][0], e1[0][1] + self.upp_bnd_elps)
 
-            # print(center)
-
-            # Drawing center
-            cv2.line(img_original, (int(center[0]), int(center[1])), (int(center[0]), int(center[1])), (0, 0, 255), 10)
+            # DEVONLY: Drawing center
+            # cv2.line(img_original, (int(center[0]), int(center[1])), (int(center[0]), int(center[1])), (0, 0, 255), 10)
 
             # Add a detected ring to the array
             if (self.scan_ranges != None):
@@ -147,9 +192,6 @@ class The_Ring:
                         endpoint=False)
                     filt = np.isnan(dpts_arg)
                     perp_agl, perp_y_itrcpt = self.get_ell_face_agl(dpts_arg[~filt], agls_arg[~filt])
-                    #t1 = dpts_arg[~filt]
-                    #t2 = agls_arg[~filt]
-                    #perp_agl, perp_y_itrcpt = self.get_ell_face_agl(np.hstack((t1[0], t1[len(t1)-1])), np.hstack((t2[0], t2[len(t2)-1])))
 
                     # Set values
                     ed.dpt.append(dpt)
@@ -163,6 +205,10 @@ class The_Ring:
             self.rings_pub.publish(ed)
 
         self.in_process = 0
+
+        # DEVONLY: Calculation time estimation per image 
+        # endTime = rospy.Time.now().to_time()
+        # print(endTime - timestamp)
 
         # DEVONLY: Visualize camera output
         cv2.imshow('Live feed', img_original)
@@ -191,10 +237,6 @@ class The_Ring:
         # Get x and y values of depth points.
         x_vals = np.cos(agls)*dpts
         y_vals = np.sin(agls)*dpts
-
-        #print("xvals: {0}".format(x_vals))
-        #print("yvals: {0}".format(y_vals))
-
 
         # interpolate line using least squares.
         k, m = np.linalg.lstsq(np.vstack((x_vals, np.ones(x_vals.shape[0]))).T, y_vals, rcond=-1)[0]
