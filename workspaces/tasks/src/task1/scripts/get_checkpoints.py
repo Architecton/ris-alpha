a5244a99f4d9f3044d8e9d5e372f6cb0a2536fc8
miha987 <zadravecmiha987@gmail.com>
2019-04-03 21:48:29 +0200

    get checkpoints

--- workspaces/tasks/src/task1/scripts/get_checkpoints.py
+++ workspaces/tasks/src/task1/scripts/get_checkpoints.py
@@ -3,15 +3,22 @@
 import rospy
 from task1.srv import Checkpoint_res
 from task1.msg import Checkpoints
-from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import OccupancyGrid, MapMetaData
 from geometry_msgs.msg import Point, Vector3, PoseStamped
 
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
 ros_map = []
+info = MapMetaData()
 
 def mapCallback(data):
     global ros_map
+    global info
 
     ros_map = data.data
+    info = data.info
 
 def req_handler(req):
 
@@ -20,7 +27,19 @@ def req_handler(req):
 
     #rospy.wait_for_service('static_map')
 
-    print ros_map
+    #print(info);
+    img = np.array(ros_map) 
+    img = img.reshape((info.height, info.width))
+
+    print info
+    #img = np.random.random((100, 100))
+
+    plt.imshow(img, cmap="gray")
+    plt.show()
+
+    #cv2.imshow("image", ros_map);
+    #cv2.waitKey();
+
     #checkpoints.points.append(p1)
     #checkpoints.points.append(p2)
     #checkpoints.points.append(p3)
