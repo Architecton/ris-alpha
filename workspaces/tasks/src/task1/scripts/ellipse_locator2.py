5bc3683147cfea02835084b7588ae34e98372111
Jernej Vivod <je.vivod@gmail.com>
2019-04-06 10:55:48 +0200

    Fix

--- workspaces/tasks/src/task1/scripts/ellipse_locator2.py
+++ workspaces/tasks/src/task1/scripts/ellipse_locator2.py
@@ -59,7 +59,7 @@ def changePermission(data):
     tm = TargetMarker()
 
     if scan_flag == 0 and buff2_ptr > 1:
-        res = np.median(buff[:buff2_ptr], 0)
+        res = np.median(buff[:buff2_ptr, :], 0)
         buff[buff_ptr] = res
         buff_ptr += 1
         buff2_ptr = 0
