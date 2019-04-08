7fdda9bc49833220eedb112df8e1407ba0ba5604
Jernej Vivod <je.vivod@gmail.com>
2019-04-08 10:45:26 +0200

    Fix

--- workspaces/tasks/src/task1/scripts/ellipse_locator4.py
+++ workspaces/tasks/src/task1/scripts/ellipse_locator4.py
@@ -166,7 +166,7 @@ def callback(data):
             # k -- tan of the angle of the line perpendicular to the face of the ellipse 
             # m -- y intercept of the line perpendicular to the face of the ellipse 
             
-            c = 0.88
+            c = 0.83
             # alternative: b = a*k + m
             #k = np.tan(data.perp_agl[ell_idx])
             #m = pos_nxt.pose.position.y - k*pos_nxt.pose.position.y 
