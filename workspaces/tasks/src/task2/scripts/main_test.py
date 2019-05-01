fc0274de9ccbb961a638743a8b5ee7d0ef3cbd34
Jernej Vivod <je.vivod@gmail.com>
2019-05-01 09:19:58 +0200

    Add sprint timeout reset functionality and add resetting of checkpoints

--- workspaces/tasks/src/task2/scripts/main_test.py
+++ workspaces/tasks/src/task2/scripts/main_test.py
@@ -49,11 +49,13 @@ class TerminalApproachHandler:
         # Initialize corrections message.
         self._corr = TerminalApproachFeedback()
         rospy.wait_for_service('terminal_approach')  # Wait for service to come online.
-	print "terminal_approach service online"
+	rospy.loginfo("terminal_approach service online")
         try:
             self._corrections_serv = rospy.ServiceProxy('terminal_approach', TerminalApproach)  # Initialize service proxy.
         except rospy.ServiceException, e:
             rospy.loginfo("Service error: {0}".format(e.message))
+
+        self._timeout_reset = False
 
 
     def _image_feedback_callback(self, data):
@@ -68,6 +70,7 @@ class TerminalApproachHandler:
 
         # Call service to apply correction.
         self._corrections_serv(self._corr)
+        self._timeout_reset = True  # If received feedback from image, reset sprint timeout.
 
 
     def sprint(self, sprint_duration, forward):
@@ -84,6 +87,9 @@ class TerminalApproachHandler:
             self._sprint_pub.publish(msg)  # Publish angular velocity.
             # msg.angular.z = -msg.angular.z
             sprint_loop_rate.sleep()
+            if self._timeout_reset:  # If _timeout_reset flag set to true, reset timeout.
+                start_time = time.time()
+                self._timeout_reset = False
 
 
     def subscribe_to_feedback(self):
@@ -122,7 +128,7 @@ class Utils:
         self._rot_pub = rospy.Publisher('/cmd_vel_mux/input/navi', Twist, queue_size=1)
 
         rospy.wait_for_service('feature_builder')  # Wait for service to come online.
-	print "feature_builder service online"
+	rospy.loginfo("feature_builder service online")
         try:
             self._feature_builder_serv = rospy.ServiceProxy('feature_builder', FeatureBuilder)  # Initialize service proxy.
         except rospy.ServiceException, e:
@@ -275,6 +281,8 @@ if __name__ == "__main__":
         [2.09860654436, 3.02047163568],
         [1.97930727727, 2.97823109257]])
 
+    checkpoints_backup = checkpoints.copy()
+
     # Define array of orientations for
     checkpoint_orientations = np.array(
         [[0.858581771733, 0.512676643946],
@@ -285,6 +293,8 @@ if __name__ == "__main__":
         [0.0828900295388, 0.996558700229],
         [-0.588117419044, 0.808775556887],
         [-0.982367777334, 0.186958685423]])
+
+    checkpoint_orientations_backup = checkpoint_orientations.copy()
     
     # Create instance of Utils class.
     ut = Utils(window_size=WINDOW_SIZE, target_center_x=TARGET_CENTER_X, terminal_approach_duration=TERMINAL_APPROACH_DURATION)
@@ -302,81 +312,67 @@ if __name__ == "__main__":
     
     # Wait for map cache to fill.
     rospy.sleep(5)
-    print "Here"
 
-    # While there are unresolved checkpoints.
-    while checkpoints.shape[0] > 0 and not done:
-	print "I am here"
-        
-        # Get robot's current position.
-
-        trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
-        robot_pos = np.array([trans.transform.translation.x, trans.transform.translation.y])
-        
-        # Get index of closest checkpoint.
-        idx_nxt = np.argmin((lambda x1, x2 : np.sum(np.abs(x1 - x2)**2.0, 1)**(1.0/2.0))(robot_pos, checkpoints))
-	rospy.loginfo("resolving next checkpoint")
-    
-        # Initialize next goal from closest checkpoint and visit it.
-        goal_chkpt = MoveBaseGoal()
-        goal_chkpt.target_pose.header.frame_id = "map"
-        goal_chkpt.target_pose.header.stamp = rospy.Time.now()
-        goal_chkpt.target_pose.pose.position.x = checkpoints[idx_nxt, 0]
-        goal_chkpt.target_pose.pose.position.y = checkpoints[idx_nxt, 1]
-        goal_chkpt.target_pose.pose.orientation.z = checkpoint_orientations[idx_nxt, 0]
-        goal_chkpt.target_pose.pose.orientation.w = checkpoint_orientations[idx_nxt, 1]
-
-        goal_chkpnt_status = GoalStatus.LOST  # Set status for next checkpoint goal.
-        ac_chkpnts.send_goal(goal_chkpt)  # Send checkpoint goal.
-
-        # Loop for next checkpoint goal.
-        while not goal_chkpnt_status == GoalStatus.SUCCEEDED:
+    while not done:
+        # While there are unresolved checkpoints.
+        while checkpoints.shape[0] > 0
             
-            # Wait 1 second for results.
-            ac_chkpnts.wait_for_result(rospy.Duration(1.0))
+            # Get robot's current position.
 
-            # mark robot trail.
-            # trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
-            # ut.mark_trail(trans)
-
-            # Get checkpoint resolution goal status.
-            goal_chkpnt_status = ac_chkpnts.get_state()
-
-            # Handle abortions.
-            if goal_chkpnt_status == GoalStatus.ABORTED or goal_chkpnt_status == GoalStatus.REJECTED:
-                rospy.loginfo("Checkpoint resolution goal aborted")
-                checkpoints = np.delete(checkpoints, (idx_nxt), axis=0)
-                checkpoint_orientations = np.delete(checkpoint_orientations, (idx_nxt), axis=0)
-                break
-
-            if goal_chkpnt_status == GoalStatus.SUCCEEDED:
-
-                ## RING DETECTION AND COLLECTION PROCEDURE ##
-                terminal_approach_performed_flg = False
-                position_resolved_flg = False
-                attempt_counter = 0
+            trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
+            robot_pos = np.array([trans.transform.translation.x, trans.transform.translation.y])
             
-                while not position_resolved_flg and attempt_counter < NUM_ATTEMPTS:
+            # Get index of closest checkpoint.
+            idx_nxt = np.argmin((lambda x1, x2 : np.sum(np.abs(x1 - x2)**2.0, 1)**(1.0/2.0))(robot_pos, checkpoints))
+            rospy.loginfo("resolving next checkpoint")
+        
+            # Initialize next goal from closest checkpoint and visit it.
+            goal_chkpt = MoveBaseGoal()
+            goal_chkpt.target_pose.header.frame_id = "map"
+            goal_chkpt.target_pose.header.stamp = rospy.Time.now()
+            goal_chkpt.target_pose.pose.position.x = checkpoints[idx_nxt, 0]
+            goal_chkpt.target_pose.pose.position.y = checkpoints[idx_nxt, 1]
+            goal_chkpt.target_pose.pose.orientation.z = checkpoint_orientations[idx_nxt, 0]
+            goal_chkpt.target_pose.pose.orientation.w = checkpoint_orientations[idx_nxt, 1]
 
-                    # See if ring detected in initial position.
-                    rospy.loginfo("detecting ring")
+            goal_chkpnt_status = GoalStatus.LOST  # Set status for next checkpoint goal.
+            ac_chkpnts.send_goal(goal_chkpt)  # Send checkpoint goal.
 
-                    detected_flg = ut.detect_ring()
-                    if detected_flg:
-                        
-                        # Mark ring.
-                        #trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
-                        #ut.mark_ring(trans, ut.depth, ut.offset_px)
+            # Loop for next checkpoint goal.
+            while not goal_chkpnt_status == GoalStatus.SUCCEEDED:
+                
+                # Wait 1 second for results.
+                ac_chkpnts.wait_for_result(rospy.Duration(1.0))
 
-                        # Perform terminal approach.
-                        ut.perform_terminal_approach()
-                        terminal_approach_performed_flg = True
-                        attempt_counter += 1  # Increment attempt counter.
-                    else:
-                        rospy.loginfo("performing scan")
-                        scan_res = ut.ring_scan(2)
-                        if scan_res:
+                # mark robot trail.
+                # trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
+                # ut.mark_trail(trans)
 
+                # Get checkpoint resolution goal status.
+                goal_chkpnt_status = ac_chkpnts.get_state()
+
+                # Handle abortions.
+                if goal_chkpnt_status == GoalStatus.ABORTED or goal_chkpnt_status == GoalStatus.REJECTED:
+                    rospy.loginfo("Checkpoint resolution goal aborted")
+                    checkpoints = np.delete(checkpoints, (idx_nxt), axis=0)
+                    checkpoint_orientations = np.delete(checkpoint_orientations, (idx_nxt), axis=0)
+                    break
+
+                if goal_chkpnt_status == GoalStatus.SUCCEEDED:
+
+                    ## RING DETECTION AND COLLECTION PROCEDURE ##
+                    terminal_approach_performed_flg = False
+                    position_resolved_flg = False
+                    attempt_counter = 0
+                
+                    while not position_resolved_flg and attempt_counter < NUM_ATTEMPTS:
+
+                        # See if ring detected in initial position.
+                        rospy.loginfo("detecting ring")
+
+                        detected_flg = ut.detect_ring()
+                        if detected_flg:
+                            
                             # Mark ring.
                             #trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
                             #ut.mark_ring(trans, ut.depth, ut.offset_px)
@@ -386,14 +382,30 @@ if __name__ == "__main__":
                             terminal_approach_performed_flg = True
                             attempt_counter += 1  # Increment attempt counter.
                         else:
-                            position_resolved_flg = True
-                            if terminal_approach_performed_flg:
-                                # TODO say that ring was successfully picked up.
-                                collected_rings_counter += 1
-                                if collected_rings_counter >= NUM_RINGS_TO_COLLECT:
-                                    done = True
+                            rospy.loginfo("performing scan")
+                            scan_res = ut.ring_scan(2)
+                            if scan_res:
 
-                checkpoints = np.delete(checkpoints, (idx_nxt), axis=0)
-                checkpoint_orientations = np.delete(checkpoint_orientations, (idx_nxt), axis=0)
-                ##############################
+                                # Mark ring.
+                                #trans = tf2_buffer.lookup_transform('map', 'base_link', rospy.Time(0))
+                                #ut.mark_ring(trans, ut.depth, ut.offset_px)
+
+                                # Perform terminal approach.
+                                ut.perform_terminal_approach()
+                                terminal_approach_performed_flg = True
+                                attempt_counter += 1  # Increment attempt counter.
+                            else:
+                                position_resolved_flg = True
+                                if terminal_approach_performed_flg:
+                                    # TODO say that ring was successfully picked up.
+                                    collected_rings_counter += 1
+                                    if collected_rings_counter >= NUM_RINGS_TO_COLLECT:
+                                        done = True
+
+                    checkpoints = np.delete(checkpoints, (idx_nxt), axis=0)
+                    checkpoint_orientations = np.delete(checkpoint_orientations, (idx_nxt), axis=0)
+                    ##############################
+        if not done:  # If visited all checkpoints and not collected all rings, reset checkpoints.
+            checkpoints = checkpoints_backup.copy()
+            checkpoint_orientations = checkpoint_orientations_backup.copy()
 
