64d8a93ae9f175e2e5e6463f3fbb01bb22accea0
Jernej Vivod <je.vivod@gmail.com>
2019-05-27 11:38:58 +0200

    Finish classifier

--- workspaces/tasks/src/task3/scripts/classification/classification.py
+++ workspaces/tasks/src/task3/scripts/classification/classification.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import numpy as np
+from sklearn.neural_network import MLPClassifier
 
 class URLDataClassifier:
 
@@ -10,26 +11,24 @@ class URLDataClassifier:
     """
    
     # Constructor
-    def __init__(learner, **kwargs):
+    def __init__(learner=MLPClassifier, **kwargs):
         self._learner = learner(**kwargs)
 
     # Fit: use learner to produce classifier.
     def fit(data_url):
         data = np.fromstring(requests.get(data_url).text)
-        self._classifier = self._learner.fit(data)
+        target = None # TODO
+        self._classifier = self._learner.fit(data, target)
         return self  # Return reference to self (to allow .fit(url).predict(example) calls)
    
     # predict: predict class of example using learned classifier.
     def predict(example):
-        return self._classifier.transform(example)
+        return self._classifier.predict(example)
 
 
 
 ## TEST ###
-
 if __name__ === '__main__':
-    from sklearn.neural_network import MLPClassifier
-
     data_url = 'http://localhost:3000'      # Url from which to load the training data.
     to_predict = np.array([2, 3])           # Example to classify.
     clf = URLDataClassifier(MLPClassifier)  # Initialize classifier.
