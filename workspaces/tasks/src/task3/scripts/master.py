2ccd21fa1d6db21f15e07cf1310706a425436e92
Vivod Jernej <vivod.jernej@gmail.com>
2019-06-05 20:07:50 +0200

    Add possibility to override outputs of stages

--- workspaces/tasks/src/task3/scripts/master.py
+++ workspaces/tasks/src/task3/scripts/master.py
@@ -5,5 +5,13 @@ from stage1 import stage_one
 
 rospy.init_node('master')
 
-res = stage_one()
-print res
+res1 = stage_one()
+correction = raw_input("Press enter if output of Stage 1 is OK, else enter correct color:")
+if correction != '':
+   res1 = correction 
+
+res2 = stage_two(res1)
+correction = raw_input("Press enter if output of Stage 2 is OK, else enter correct color:")
+if correction != '':
+    res2 = correction
+
