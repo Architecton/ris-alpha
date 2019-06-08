5941d713b7c8bec3e9e45a5b12c967c14691f53f
Jernej Vivod <je.vivod@gmail.com>
2019-06-08 16:27:56 +0200

    work on sound client

--- workspaces/tasks/src/task3/scripts/sound/sound_client.py
+++ workspaces/tasks/src/task3/scripts/sound/sound_client.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import rospy
+from task3.msg import SayCommand
 
 if __name__ == '__main__':
     rospy.init_node('test', anonymous=True)
-    print "tralala"
+    say_pub = rospy.Publisher('say_commands', SayCommand, queue_size = 10)
+    say_pub.publish('red')
