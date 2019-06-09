454235915d1199ef2bfc13b1b0e8ed671aa1c58d
dkalsan <damjankalsan@gmail.com>
2019-06-09 08:39:08 +0200

    param change

--- workspaces/tasks/src/task3/src/cylinder_recognition/cylinder_recognition.cpp
+++ workspaces/tasks/src/task3/src/cylinder_recognition/cylinder_recognition.cpp
@@ -155,7 +155,7 @@ void cloud_cb (const pcl::PCLPointCloud2ConstPtr& cloud_blob) {
     // n = 3
     // w = 0.21
     // 99% chance of finding a cylinder even if approx 81% of all points are outliers
-    seg.setMaxIterations (1200);
+    seg.setMaxIterations (1500);
     seg.setDistanceThreshold (0.25);
     seg.setRadiusLimits (0.1, 0.18);
     seg.setInputCloud (cloud_filtered2);
